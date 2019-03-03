e12196d2eae80beb8e83e4b2baa387345c9d4c2c
shortymolinari <shortymolinari@gmail.com>
2019-03-03 13:57:58 -0500

    Implements Multiton, Design Patterns

---
+++ DesignPatterns/Creational/Multiton/Multiton.md
@@ -0,0 +1,15 @@
+## \# Multiton 
+>:sparkles: ***Design Patters | Creational***
+
+***THIS IS CONSIDERED TO BE AN ANTI-PATTERN! FOR BETTER TESTABILITY AND MAINTAINABILITY USE DEPENDENCY INJECTION!***
+
+---
+####\#Propósito
+- Para tener solo una lista de las instancias con nombre que se utilizan, como un singleton pero con ***n*** instancias.
+
+####\#Ejemplos
+- 2 conectores DB, por ejemplo, Una para MySQL, la otra para SQLite.
+- Múltiples registradores (uno para mensajes de depuración, uno para errores).
+
+![GitHub Logo](https://designpatternsphp.readthedocs.io/en/latest/_images/uml15.png)
+
---
+++ test/Creational/Multiton/MultitonTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Creational\Multition;
+
+use PHPUnit\Framework\TestCase;
+use DesignPatterns\Creational\Multiton\Multiton;
+
+class MultitonTest extends TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->firstCall = Multiton::getInstance(Multiton::INSTANCE_1);
+        $this->secondCall = Multiton::getInstance(Multiton::INSTANCE_1);
+        $this->thirdCall = Multiton::getInstance(Multiton::INSTANCE_2);
+    }
+
+    /**
+     * @group Multiton
+     * @test
+     */
+    public function uiqueness()
+    {
+        $this->assertInstanceOf(Multiton::class, $this->firstCall);
+        $this->assertSame($this->firstCall, $this->secondCall);
+    }
+
+    /**
+     * @group Multiton
+     * @test
+     */
+    public function uniqueness_for_every_instance()
+    {
+        $this->assertInstanceOf(Multiton::class, $this->firstCall);
+        $this->assertInstanceOf(Multiton::class, $this->thirdCall);
+        $this->assertNotSame($this->firstCall, $this->thirdCall);
+    }
+}
