3b116e5f05dfaba35e682bcf66807519efee3fd8
Edwin Arley Rincon Henao <shortymolinari@gmail.com>
2019-03-14 15:21:35 -0500

    Ejemplo #2 teamaker

---
+++ DesignPatterns/Structural/Flyweight/FlyweightTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DesignPatterns\Structural\Flyweight;
+
+
+use DesignPatterns\Structural\Flyweight\FlyweightFactory;
+require '../../../vendor/autoload.php';
+
+class FlyweightTest
+{
+    private $characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    private $fonts = ['Arial', 'Times New Roman', 'Verdana', 'Helvetica'];
+
+    public function testFlyweight()
+    {
+        $factory = new FlyweightFactory();
+
+        foreach ($this->characters as $char) {
+            foreach ($this->fonts as $font) {
+                $flyweight = $factory->get($char);
+                $rendered = $flyweight->render($font);
+
+                sprintf('Character %s with font %s', $char, $font);
+            }
+        }
+
+        // Flyweight pattern ensures that instances are shared
+        // instead of having hundreds of thousands of individual objects
+        // there must be one instance for every char that has been reused for displaying in different fonts
+        //$this->assertCount(count($this->characters), $factory);
+    }
+}
+
+$flyweightTest = new FlyweightTest;
+echo $flyweightTest->testFlyweight();
