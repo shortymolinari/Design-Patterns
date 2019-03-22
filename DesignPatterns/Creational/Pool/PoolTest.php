4befea667839bb6bcaf28585d7cac609180e45b8
Edwin Arley Rincon Henao <shortymolinari@gmail.com>
2019-03-22 13:21:51 -0500

    Agregando Pool Design Pattern

---
+++ DesignPatterns/Creational/Pool/PoolTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DesignPatterns\Creational\Pool;
+
+use DesignPatterns\Creational\Pool\WorkerPool;
+
+class PoolTest {
+
+    public function testCanGetNewInstancesWithGet()
+    {
+        $pool = new WorkerPool();
+        $worker1 = $pool->get();
+        $worker2 = $pool->get();
+
+        //$this->assertCount(2, $pool);
+        //$this->assertNotSame($worker1, $worker2);
+    }
+
+    public function testCanGetSameInstanceTwiceWhenDisposingItFirst()
+    {
+        $pool = new WorkerPool();
+        $worker1 = $pool->get();
+        $pool->dispose($worker1);
+        $worker2 = $pool->get();
+
+        $this->assertCount(1, $pool);
+        $this->assertSame($worker1, $worker2);
+    }
+}
---
+++ DesignPatterns/Creational/Pool/README.md
@@ -0,0 +1,8 @@
+## \# Pool 
+>:sparkles: ***Design Patters | Structural***
+---
+### Propósito
+
+
+#### Diagrama
+![Pool](https://designpatternsphp.readthedocs.io/en/latest/_images/uml16.png)
