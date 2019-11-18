ca40f4810b6aae46053985a90612254ee723987a
Edwin Rincon <desarrollo2@diezequis.com>
2019-11-18 09:48:06 -0500

    Static Factory Design Paterrn update

--- DesignPatterns/Creational/StaticFactory/Client.php
+++ DesignPatterns/Creational/StaticFactory/Client.php
@@ -6,15 +6,17 @@ use DesignPatterns\Creational\StaticFactory\StaticFactory;
 use DesignPatterns\Creational\StaticFactory\FormatNumber;
 use DesignPatterns\Creational\StaticFactory\FormatString;
 
+
 require "../../../vendor/autoload.php";
 
+
 if (StaticFactory::factory('number') instanceof FormatNumber) {
-    echo "Number";
+    echo "Number Format" . PHP_EOL;
 }
 
 
 if (StaticFactory::factory('string') instanceof FormatString) {
-    echo "String";
+    echo "String Format" . PHP_EOL;
 } 
 
 try {
